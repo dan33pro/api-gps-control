a2b6c09b1e023a3a7cbfe4d7bc1fccd6b98f39ea
DagoGres <dan333pro@gmai.com>
2024-07-25 17:50:41 -0500

    feat: Solo peticiones desde el front y ordenamiento por id en get

--- api_gps_control/settings.py
+++ api_gps_control/settings.py
@@ -140,7 +140,11 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://gpscontrol.vercel.app/",
+]
+
 CORS_ALLOW_METHODS = [
     "DELETE",
     "GET",
